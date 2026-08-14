49f1dd30026f5d553f84a82dfb031e168d301766
adr14n1109 <adrianremolina11@gmail.com>
2026-08-14 11:32:40 -0500

    bug corregido

--- Taller 1/src/MisAlgoritmos.java
+++ Taller 1/src/MisAlgoritmos.java
@@ -2,7 +2,7 @@ public class MisAlgoritmos implements Busquedas,Ordenamientos {
     @Override
     public int linearSearch(int[] arrayPorExplorar, int elementoABuscar) {
        int tamanoLista = arrayPorExplorar.length;
-       for (int i = 0; i < tamanoLista-1; i++) {
+       for (int i = 0; i < tamanoLista; i++) {
             int elementoActual = arrayPorExplorar[i];
             if(elementoActual == elementoABuscar){
                 return i;
@@ -14,9 +14,12 @@ public class MisAlgoritmos implements Busquedas,Ordenamientos {
 
     @Override
     public int binarySearch(int[] arrayPorExplorar, int elementoABuscar) {
+        int tamanoLista = arrayPorExplorar.length;
+        for (int i = 0; i < ; i++) {
+            
+        }
         return 0;
     }
-
     @Override
     public int[] bubbleSort(int[] arrayDesordenado) {
         return new int[0];
