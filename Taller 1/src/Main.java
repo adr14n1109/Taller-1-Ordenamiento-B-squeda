a65b66cea6a1f5fca7a7a8cb45f90c0cdf75c610
adr14n1109 <adrianremolina11@gmail.com>
2026-08-14 11:16:59 -0500

    Cree proyecto e implemente el metodo de busqueda linear

---
+++ Taller 1/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] miArray = {70, 12, 19, 54, 11, 33};
+        MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
+
+        int posicion = misAlgoritmos.linearSearch(miArray, 12);
+
+        System.out.println(posicion);
+    }
+}
---
+++ Taller 1/src/MisAlgoritmos.java
@@ -0,0 +1,44 @@
+public class MisAlgoritmos implements Busquedas,Ordenamientos {
+    @Override
+    public int linearSearch(int[] arrayPorExplorar, int elementoABuscar) {
+       int tamanoLista = arrayPorExplorar.length;
+       for (int i = 0; i < tamanoLista-1; i++) {
+            int elementoActual = arrayPorExplorar[i];
+            if(elementoActual == elementoABuscar){
+                return i;
+            }
+
+       }
+        return -1;
+    }
+
+    @Override
+    public int binarySearch(int[] arrayPorExplorar, int elementoABuscar) {
+        return 0;
+    }
+
+    @Override
+    public int[] bubbleSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] selectionSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] insertionSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] mergeSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] quickSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+}
