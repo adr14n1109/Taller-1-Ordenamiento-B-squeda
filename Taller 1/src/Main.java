a082e38780a5abfd8c76cca30b6d181086f07f51
adr14n1109 <adrianremolina11@gmail.com>
2026-08-21 11:39:29 -0500

    Termine BinarySearch

--- Taller 1/src/Main.java
+++ Taller 1/src/Main.java
@@ -1,12 +1,23 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] miArray = {70, 12, 19, 45, 11, 33, 26};
+        //int[] miArray = {70, 12, 19, 45, 11, 33, 26};
         MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
 
-        int posicion = misAlgoritmos.linearSearch(miArray, 12);
+        //int posicion = misAlgoritmos.linearSearch(miArray, 12);
 
+        //System.out.println(posicion);
+
+
+        int[] miArray = {11,12,19,26,33,45,70};
+
+        int posicion = misAlgoritmos.binarySearch(miArray,12);
         System.out.println(posicion);
+
+
     }
 }
+
--- Taller 1/src/MisAlgoritmos.java
+++ Taller 1/src/MisAlgoritmos.java
@@ -1,25 +1,37 @@
-public class MisAlgoritmos implements Busquedas,Ordenamientos {
+public class MisAlgoritmos implements Busquedas, Ordenamientos {
     @Override
     public int linearSearch(int[] arrayPorExplorar, int elementoABuscar) {
-       int tamanoLista = arrayPorExplorar.length;
-       for (int i = 0; i < tamanoLista; i++) {
+        int tamanoLista = arrayPorExplorar.length;
+        for (int i = 0; i < tamanoLista; i++) {
             int elementoActual = arrayPorExplorar[i];
-            if(elementoActual == elementoABuscar){
+            if (elementoActual == elementoABuscar) {
                 return i;
             }
 
-       }
+        }
         return -1;
     }
 
     @Override
     public int binarySearch(int[] arrayPorExplorar, int elementoABuscar) {
-        int valorMitad = arrayPorExplorar.length/2;
-        for (int i = 0; i < ; i++) {
-            
+        int inicio = 0;
+        int fin = arrayPorExplorar.length - 1;
+
+        while (inicio <= fin) {
+            int medio = inicio + (fin - inicio) / 2;
+
+            if (arrayPorExplorar[medio] == elementoABuscar) {
+                return medio;
+            } else if (arrayPorExplorar[medio] < elementoABuscar) {
+                inicio = medio + 1;
+            } else {
+                fin = medio - 1;
+            }
         }
-        return 0;
+
+        return -1;
     }
+
     @Override
     public int[] bubbleSort(int[] arrayDesordenado) {
         return new int[0];
